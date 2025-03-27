664142aaf8b1b7d772592af50b5e8ec72cc6c3a8
Shih-Yi Hsiung <shsiung2019@gmail.com>
2025-03-27 11:29:07 +0900

    Update Data organization (csv).py

--- Data organization (csv).py
+++ Data organization (csv).py
@@ -14,3 +14,34 @@ merged_df = c_df.merge(b_df, on='GeneID', how='left')  # Use left join to preser
 merged_df.to_csv('merged.csv', index=False) #Replace with your file path
 
 print("Merge complete. Output saved to merged.csv")
+
+
+
+
+
+
+----
+
+# Remove or add elements in a column
+
+import pandas as pd
+
+# Load the CSV file
+input_file = "Species_with_class_order.csv"  # Ensure correct path
+output_file = "Species_with_class_order_modified.csv"   # Ensure correct path
+
+df = pd.read_csv(input_file)
+
+# Remove '(' and ')' from column 'Glycan_structure', handling NaN values safely
+df['Glycan_structure'] = df['Glycan_structure'].astype(str).str.replace(r'[()]', '', regex=True)
+
+# Remove '-like' from column 'Glycan_structure', handling NaN values safely
+df['Glycan_structure'] = df['Glycan_structure'].astype(str).str.replace(r'[-like]', '', regex=True)
+
+
+# Save to a new CSV file
+df.to_csv(output_file, index=False)
+
+print(f"Modified file saved as {output_file}")
+
+
